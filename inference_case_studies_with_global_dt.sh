55a524762348ab553f46d9c959f6e98e7e3157a0
ealerskans <ea@dmi.dk>
2025-05-20 14:24:33 +0000

    Add weather-model-graphs and update neural-lam args

--- inference_case_studies_with_global_dt.sh
+++ inference_case_studies_with_global_dt.sh
@@ -22,21 +22,28 @@ saved_model=/dmidata/users/ea/neural-lam/data/saved_model/train-hi_lam-2x300-02_
 neural_lam_config=/dmidata/users/ea/neural-lam/data/config.yaml
 
 # 1.3 Get the command (line arguemnts) used to train the model
-graph_name=3deg_rect_hi3_new
+graph_name=rect_hi3
 neural_lam_command_line_args=(
     --hidden_dim 300
+    --hidden_dim_grid 150
     --num_workers 6
     --precision bf16-mixed
     --batch_size 1
+    --time_delta_enc_dim 32
+    --processor_layers 2
+    --dynamic_time_deltas
     --config_path ${neural_lam_config}
     --model hi_lam
     --graph ${graph_name}
-    --val_steps_to_log 2
+    --val_steps_to_log 1 2 4
     --load ${saved_model}
     --epochs 80
     --ar_steps_train 1
     --lr 0.001
-    --ar_steps_eval 2
+    --min_lr 0.001
+    --val_interval 5
+    --ar_steps_eval 4
+    --num_nodes 2
 )
 
 # Step 2:
@@ -56,7 +63,7 @@ cd ${wd}
 
 # Step 3:
 #=========
-# 3.1 Data preparation and neural-lam: Clone git repositories and install environments 
+# 3.1 Data preparation, graph creation and neural-lam: Clone git repositories and install environments 
 # 3.1.1 mllam-data-prep
 mllam_data_prep_git_repo=https://github.com/ealerskans/mllam-data-prep.git
 mllam_data_prep_branch=dk-case-studies-with-global-dt
@@ -66,7 +73,16 @@ pdm venv create
 pdm use --venv in-project
 pdm install -G latlon-domain-crop
 cd ${wd}
-# 3.1.2 neural-lam
+# 3.1.2 weather-model-graphs
+weather_model_graphs_git_repo=https://github.com/ealerskans/mllam-data-prep.git
+weather_model_graphs_branch=building-ml-lams
+git clone --single-branch -b ${weather_model_graphs_branch} ${weather_model_graphs_git_repo} ${git_root_dir}/weather-model-graphs
+cd ${git_root_dir}/weather-model-graphs
+pdm venv create
+pdm use --venv in-project
+pdm install --dev
+cd ${wd}
+# 3.1.3 neural-lam
 neural_lam_git_repo=https://github.com/ealerskans/neural-lam.git
 neural_lam_branch=neural-lam-dev-research
 git clone --single-branch -b ${neural_lam_branch} ${neural_lam_git_repo} ${git_root_dir}/neural-lam
@@ -89,13 +105,14 @@ cd ${wd}
 # 5.1 Create graph - Hierarchical 3-level
 cd ${git_root_dir}/neural-lam
 lev=3
-MSD=1.85
+#MND=12500
+MND=1.85
 ${PYTHON} -m neural_lam.build_rectangular_graph \
   --config_path ${neural_lam_config} \
   --archetype hierarchical \
-  --max_num_levels $lev \
-  --graph_name ${graph_name} \
-  --mesh_node_distance $MSD
+  --max_num_levels ${lev} \
+  --mesh_node_distance ${MND} \
+  --graph_name ${graph_name}
 cd ${wd}
 
 # Step 6:
